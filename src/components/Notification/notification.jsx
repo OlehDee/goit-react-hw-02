fa57beea1ff55ba806cc356f70dfdbe1b41d8410
Oleh_Denysenko <Oleh.Denysenkoo@gmail.com>
2025-04-26 12:24:50 +0300

    N to n

--- src/components/Notification/notification.jsx
+++ src/components/Notification/notification.jsx
@@ -1,4 +1,4 @@
-import styles from "./Notification.module.css";
+import styles from "./notification.module.css";
 
 const Notification = ({ message }) => {
   return <div className={styles.notification}>{message}</div>;
